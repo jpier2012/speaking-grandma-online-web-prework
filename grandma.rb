df3022649694270af43104ad1f2ca57de1699ad2
James Pier <jpier2012@gmail.com>
2019-04-01 00:49:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,16 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma statement 
+  if statement != statement.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  elsif statement == "I LOVE YOU, GRANDMA!"
+      "I LOVE YOU TOO PUMPKIN!"
+  else 
+    "NO, NOT SINCE 1938!"
+end
+
+speak_to_grandma "ALL CAPS" 
+speak_to_grandma "lowercase" 
+speak_to_grandma "I LOVE YOU, GRANDMA!"  
