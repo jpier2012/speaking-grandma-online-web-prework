c045ffe0b8c92c52fdbbf8be2ea1cace8078ea9a
James Pier <jpier2012@gmail.com>
2019-04-01 00:49:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -12,15 +12,15 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma statement 
+def speak_to_grandma statement
   if statement != statement.upcase
     "HUH?! SPEAK UP, SONNY!"
   elsif statement == "I LOVE YOU, GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
-  else 
+  else
     "NO, NOT SINCE 1938!"
 end
 
-speak_to_grandma "ALL CAPS" 
-speak_to_grandma "lowercase" 
+speak_to_grandma "ALL CAPS"
+speak_to_grandma "lowercase"
 speak_to_grandma "I LOVE YOU, GRANDMA!"  
